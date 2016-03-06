e7342ffbb739410502a6d4f3d7545e41252c79e1
kraynel <kraynel@yahoo.fr>
2016-03-06 20:31:02 +0100

    Update 2016-03-06T19:30:46.776Z

--- gulp/tasks/default.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-// Default task
-module.exports = gulp.task('default', ['js', 'jade', 'notes', 'stylus', 'build-image', 'watch', 'browser-sync', 'js']);
