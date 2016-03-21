1cb3cb2cd4663d127f79695171e270134302b91b
Kevin Raynel <kevinr@theodo.fr>
2016-03-21 15:04:51 +0100

    Update 2016-03-21T14:03:48.082Z

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-Reveal.initialize({controls:!0,progress:!0,history:!0,center:!0,transition:"fade",backgroundTransition:"none",dependencies:[{src:"/js/vendor/classList.js",condition:function(){return!document.body.classList}},{src:"js/vendor/notes/notes.js",async:!0}]});
+Reveal.initialize({controls:!0,progress:!0,history:!0,center:!0,slideNumber:!0,transition:"fade",backgroundTransition:"none",dependencies:[{src:"/js/vendor/classList.js",condition:function(){return!document.body.classList}},{src:"js/vendor/notes/notes.js",async:!0}]});
